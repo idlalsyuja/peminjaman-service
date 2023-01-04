000d3247d1b85a0cede7777f0402072f858018b0
IDLAL SYUJA NIZHAMUL <113502244+idlalsyuja@users.noreply.github.com>
2023-01-04 20:17:35 +0700

    Add files via upload

---
+++ src/main/java/com/idlal/peminjaman/VO/ResponseTemplateVO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.idlal.peminjaman.VO;
+
+import com.idlal.peminjaman.entity.Peminjaman;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author user
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+            
+}
